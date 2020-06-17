4db6ac725a067a2301e734e166ddfeea35425bd4
hayaalameeri <hayoony.m@gmail.com>
2020-06-17 20:44:17 +0300

    finished the cw

---
+++ iOSClasswork2/ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,16 @@
+let family = ["Haya","Saleh","Bedour","Mohammad", "Shaikha"]
+
+var evens = [2, 4, 6, 8, 10, 12]
+
+var grades = [100,100,99.7]
+
+var trueFalse = [true,true,true,false ]
+
+print(family[0], family [4])
+
+print(evens[0],evens[5])
+
+print(grades[0], grades[2])
+
+print(trueFalse[0], trueFalse[3])
+
